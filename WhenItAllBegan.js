abb0455dd5f9098bfcce13052bb2e75770c84031
Celine <celinelafosse218@gmail.com>
2025-10-28 14:56:45 -0700

    hidden link

---
+++ WhenItAllBegan.js
@@ -0,0 +1,9 @@
+var password = "2036";
+function passcheck () {
+    if(document.getElementById('pass1').value != password) {
+        alert('incorrect'); 
+    }
+    if(document.getElementById('pass1').value = password) {
+        alert('correct'); 
+    }
+}
