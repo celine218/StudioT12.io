a2a422b3c8f0fab0af5a18fe33ad44677644057c
Celine <celinelafosse218@gmail.com>
2025-11-13 18:57:54 -0800

    PasswordPrtected

--- WhenItAllBegan.js
+++ WhenItAllBegan.js
@@ -1,9 +1,10 @@
 var password = "2036";
 function passcheck () {
-    if(document.getElementById('pass1').value != password) {
-        alert('incorrect'); 
+if(document.getElementById('pass1').value != password) {
+    alert('When it all began...'); 
+    return false;
     }
     if(document.getElementById('pass1').value = password) {
-        alert('correct'); 
+        alert('Time stamps of tragedy spread across different perspectives'); 
     }
 }
